fae93c25eea4d7c100ffdab06340a8d75a22a8f9
GhavinJ <ghavinj@icloud.com>
2019-05-14 21:53:05 -0400

    Added comment explaining button Onclick function!

--- app/src/main/java/com/ghavinj/quicklauncher/MainActivity.java
+++ app/src/main/java/com/ghavinj/quicklauncher/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ghavinj.quicklauncher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Attempt to launch activity from within our own app
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),SecondActivity.class);
+                //show how to pass information to another activity
+                startIntent.putExtra("com.ghavinj.quicklauncher.SOMETHING", "Now Goof Troop!");
+                startActivity(startIntent);
+            }
+        });
     }
 }
